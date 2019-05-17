ba0d8c93a555db0bf181204301bc42194435de9d
kaisheng.yang <kaisheng.yang@semidrive.com>
2019-05-17 14:34:44 +0800

    [display] add display driver
    Change-Id: Ia3667819d710fefd192c2a4a3ffbba7506e9214c
    Signed-off-by: kaisheng.yang <kaisheng.yang@semidrive.com>

---
+++ drivers/gpu/drm/panel/panel-kunlun-simple.c
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2015 Heiko Schocher <hs@denx.de>
+ *
+ * from:
+ * drivers/gpu/drm/panel/panel-ld9040.c
+ * ld9040 AMOLED LCD drm_panel driver.
+ *
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd
+ * Derived from drivers/video/backlight/ld9040.c
+ *
+ * Andrzej Hajda <a.hajda@samsung.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <drm/drmP.h>
+#include <drm/drm_panel.h>
+
+#include <video/of_videomode.h>
+#include <video/videomode.h>
+
+struct kpanel {
+	struct drm_panel panel;
+	struct videomode vm;
+};
+
+static inline struct kpanel *panel_to_kpanel(struct drm_panel *panel)
+{
+	return container_of(panel, struct kpanel, panel);
+}
+
+static int kpanel_display_on(struct kpanel *ctx)
+{
+	return 0;
+}
+
+static int kpanel_display_off(struct kpanel *ctx)
+{
+	return 0;
+}
+
+static int kpanel_power_on(struct kpanel *ctx)
+{
+	return kpanel_display_on(ctx);
+}
+
+static int kpanel_disable(struct drm_panel *panel)
+{
+	struct kpanel *ctx = panel_to_kpanel(panel);
+
+	return kpanel_display_off(ctx);
+}
+
+static int kpanel_enable(struct drm_panel *panel)
+{
+	struct kpanel *ctx = panel_to_kpanel(panel);
+
+	return kpanel_power_on(ctx);
+}
+
+static const struct drm_display_mode default_mode = {
+	.clock = 74250,
+	.hdisplay = 1280,
+	.hsync_start = 1280 + 70,
+	.hsync_end = 1280 + 70 + 40,
+	.htotal = 1280 + 70 + 40 + 260,
+	.vdisplay = 720,
+	.vsync_start = 720 + 20,
+	.vsync_end = 720 + 20 + 5,
+	.vtotal = 720 + 20 + 5 + 5,
+	.vrefresh = 60,
+};
+
+static int kpanel_get_modes(struct drm_panel *panel)
+{
+	struct drm_connector *connector = panel->connector;
+	struct drm_display_mode *mode;
+
+	mode = drm_mode_duplicate(panel->drm, &default_mode);
+	if (!mode) {
+		dev_err(panel->drm->dev, "failed to add mode %ux%ux@%u\n",
+			default_mode.hdisplay, default_mode.vdisplay,
+			default_mode.vrefresh);
+		return -ENOMEM;
+	}
+
+	drm_mode_set_name(mode);
+
+	mode->type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED;
+	drm_mode_probed_add(connector, mode);
+
+	panel->connector->display_info.width_mm = 61;
+	panel->connector->display_info.height_mm = 103;
+
+	return 1;
+}
+
+static const struct drm_panel_funcs kpanel_drm_funcs = {
+	.disable = kpanel_disable,
+	.enable = kpanel_enable,
+	.get_modes = kpanel_get_modes,
+};
+
+static int kpanel_probe(struct platform_device *pdev)
+{
+	struct kpanel *ctx;
+
+	ctx = devm_kzalloc(&pdev->dev, sizeof(*ctx), GFP_KERNEL);
+	if (!ctx)
+		return -ENOMEM;
+
+	drm_panel_init(&ctx->panel);
+	ctx->panel.dev = &pdev->dev;
+	ctx->panel.funcs = &kpanel_drm_funcs;
+
+	platform_set_drvdata(pdev, ctx);
+	return drm_panel_add(&ctx->panel);
+}
+
+static int kpanel_remove(struct platform_device *pdev)
+{
+	struct kpanel *ctx = platform_get_drvdata(pdev);
+
+	kpanel_display_off(ctx);
+	drm_panel_remove(&ctx->panel);
+
+	return 0;
+}
+
+static const struct of_device_id kpanel_of_match[] = {
+	{ .compatible = "semidrive,panel-simple" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, kpanel_of_match);
+
+static struct platform_driver panel_lvds_driver = {
+	.probe		= kpanel_probe,
+	.remove		= kpanel_remove,
+	.driver		= {
+		.name	= "kunlun-panel-simple",
+		.of_match_table = kpanel_of_match,
+	},
+};
+
+module_platform_driver(panel_lvds_driver);
+
+MODULE_AUTHOR("Heiko Schocher <hs@denx.de>");
+MODULE_DESCRIPTION("kpanel LCD Driver");
+MODULE_LICENSE("GPL v2");
