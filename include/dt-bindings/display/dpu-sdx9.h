9b862f13721137be5f983a9327b021715bbebbe3
kun.zhang <kun.zhang@semidrive.com>
2019-08-07 14:35:26 +0800

    [display] Optimize display dts configuration. 1.Split common and project-specific congigs. 2.Change DP&DC combination mode. 3.Add double display support.
    Change-Id: If00d30aeae7f12aa43ac9b3762f933922c00b055
    Signed-off-by: kun.zhang <kun.zhang@semidrive.com>

---
+++ include/dt-bindings/display/dpu-sdx9.h
@@ -0,0 +1,21 @@
+/*
+* Copyright (c) 2019 Semidrive Semiconductor.
+* All rights reserved.
+*
+*/
+
+#ifndef __DTS_SDX9_DPU_H
+#define __DTS_SDX9_DPU_H
+
+#define DP0_BASE    0x0 0x30d80000
+#define DP1_BASE    0x0 0x30d90000
+#define DP2_BASE    0x0 0x30da0000
+
+#define DC0_BASE    0x0 0x30db0000
+#define DC1_BASE    0x0 0x30dc0000
+#define DC2_BASE    0x0 0x30dd0000
+#define DC3_BASE    0x0 0x30de0000
+#define DC4_BASE    0x0 0x30df0000
+
+#define DPU_RANGE   0x0 0x10000
+#endif /*__DTS_SDX9_DPU_H*/
