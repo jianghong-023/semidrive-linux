1627cd4944d0aabe531658d478530529f1874b29
li.dou <li.dou@semidrive.com>
2020-04-23 16:29:28 +0800

    [PCIe] add pcie controller driver
    Change-Id: I52979cb922082cb27d982f54f26e0bad6459a0bc

---
+++ drivers/pci/dwc/pcie-sd.c
@@ -0,0 +1,573 @@
+/*
+ * PCIe host controller driver for Semidrive
+ *
+ *
+ *
+ *
+ * Author:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <asm/compiler.h>
+#include <linux/clk.h>
+#include <linux/compiler.h>
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <linux/mfd/syscon.h>
+#include <linux/of_address.h>
+#include <linux/of_gpio.h>
+#include <linux/of_pci.h>
+#include <linux/pci.h>
+#include <linux/pci_regs.h>
+#include <linux/platform_device.h>
+#include <linux/regmap.h>
+#include <linux/resource.h>
+#include <linux/types.h>
+
+#include "pcie-designware.h"
+
+#define to_sd_pcie(x) dev_get_drvdata((x)->dev)
+
+#define SD_PCIE_ATU_OFFSET (0xc0000)
+
+/* PCIe PHY NCR registers */
+#define PCIE_PHY_NCR_CTRL0 (0x0)
+#define PCIE_PHY_NCR_CTRL1 (0x4)
+#define PCIE_PHY_NCR_CTRL4 (0x10)
+#define PCIE_PHY_NCR_CTRL15 (0x3c)
+#define PCIE_PHY_NCR_CTRL25 (0x64)
+#define PCIE_PHY_NCR_STS0 (0x80)
+
+/* info in PCIe PHY NCR registers */
+#define CR_ADDR_MODE_BIT (0x1 << 29)
+#define BIF_EN_BIT (0x1 << 1)
+#define PHY_REF_USE_PAD_BIT (0x1 << 5)
+#define PHY_REF_ALT_CLK_SEL_BIT (0x1 << 8)
+
+#define PHY_RESET_BIT (0x1 << 0)
+#define CR_CKEN_BIT (0x1 << 24)
+
+/* PCIe CTRL NCR registers */
+#define PCIE_CTRL_NCR_INTR0 (0x0)
+#define PCIE_CTRL_NCR_INTEN0 (0x34)
+#define PCIE_CTRL_NCR_INTEN1 (0x38)
+#define PCIE_CTRL_NCR_INTEN2 (0x3c)
+#define PCIE_CTRL_NCR_INTEN3 (0x40)
+#define PCIE_CTRL_NCR_INTEN4 (0x44)
+#define PCIE_CTRL_NCR_INTEN5 (0x48)
+#define PCIE_CTRL_NCR_INTEN6 (0x4c)
+#define PCIE_CTRL_NCR_INTEN7 (0x50)
+#define PCIE_CTRL_NCR_INTEN8 (0x54)
+#define PCIE_CTRL_NCR_INTEN9 (0x58)
+#define PCIE_CTRL_NCR_INTEN10 (0x5c)
+#define PCIE_CTRL_NCR_INTEN11 (0x60)
+#define PCIE_CTRL_NCR_INTEN12 (0x64)
+#define PCIE_CTRL_NCR_CTRL0 (0x68)
+#define PCIE_CTRL_NCR_CTRL2 (0x70)
+#define PCIE_CTRL_NCR_CTRL21 (0xbc)
+#define PCIE_CTRL_NCR_CTRL22 (0xc0)
+#define PCIE_CTRL_NCR_CTRL23 (0xc4)
+#define PCIE_CTRL_NCR_CTRL24 (0xc8)
+
+/* info in PCIe CTRL NCR registers */
+#define APP_LTSSM_EN_BIT (0x1 << 2)
+#define DEVICE_TYPE_BIT (0x1 << 3)
+#define APP_HOLD_PHY_RST_BIT (0x1 << 6)
+#define INTR_SMLH_LINK_UP (0x1 << 28)
+#define INTR_RDLH_LINK_UP (0x1 << 27)
+
+/* Time for delay */
+#define TIME_PHY_RST_MIN (5)
+#define TIME_PHY_RST_MAX (10)
+#define LINK_WAIT_MIN (900)
+#define LINK_WAIT_MAX (1000)
+
+#define NO_PCIE (0)
+#define PCIE1_INDEX (1)
+#define PCIE2_INDEX (2)
+
+#define PHY_REFCLK_USE_INTERNAL (0x1)
+#define PHY_REFCLK_USE_EXTERNAL (0x2)
+#define PHY_REFCLK_USE_DIFFBUF (0x3)
+#define PHY_REFCLK_USE_MASK (0x3)
+#define DIFFBUF_OUT_EN (0x1 << 31)
+
+#define LANE_CFG(lane1, lane0) (lane0 | lane1 << 16)
+#define GET_LANE0_CFG(val) (val & 0xffff)
+#define GET_LANE1_CFG(val) (val >> 16)
+
+enum sd_pcie_lane_cfg {
+	PCIE1_WITH_LANE0 = LANE_CFG(NO_PCIE, PCIE1_INDEX),
+	PCIE1_WITH_LANE1 = LANE_CFG(PCIE1_INDEX, NO_PCIE),
+	PCIE1_WITH_ALL_LANES = LANE_CFG(PCIE1_INDEX, PCIE1_INDEX),
+	PCIE2_WITH_LANE1 = LANE_CFG(PCIE2_INDEX, NO_PCIE),
+	PCIE1_2_EACH_ONE_LANE = LANE_CFG(PCIE2_INDEX, PCIE1_INDEX),
+};
+
+struct sd_pcie {
+	struct dw_pcie *pcie;
+	struct clk *pcie_aclk;
+	struct clk *pcie_pclk;
+	struct clk *phy_ref;
+	struct clk *phy_pclk;
+	void __iomem *phy_base;
+	void __iomem *phy_ncr_base;
+	void __iomem *ctrl_ncr_base;
+	enum sd_pcie_lane_cfg lane_config;
+	u32 phy_refclk_sel;
+};
+
+static inline void sd_apb_phy_writel(struct sd_pcie *sd_pcie,
+					 u32 val, u32 reg)
+{
+	writel(val, sd_pcie->phy_base + reg);
+}
+
+static inline u32 sd_apb_phy_readl(struct sd_pcie *sd_pcie, u32 reg)
+{
+	return readl(sd_pcie->phy_base + reg);
+}
+
+static inline void sd_phy_ncr_writel(struct sd_pcie *sd_pcie,
+					 u32 val, u32 reg)
+{
+	writel(val, sd_pcie->phy_ncr_base + reg);
+}
+
+static inline u32 sd_phy_ncr_readl(struct sd_pcie *sd_pcie, u32 reg)
+{
+	return readl(sd_pcie->phy_ncr_base + reg);
+}
+
+static inline void sd_ctrl_ncr_writel(struct sd_pcie *sd_pcie,
+					  u32 val, u32 reg)
+{
+	writel(val, sd_pcie->ctrl_ncr_base + reg);
+}
+
+static inline u32 sd_ctrl_ncr_readl(struct sd_pcie *sd_pcie,
+					u32 reg)
+{
+	return readl(sd_pcie->ctrl_ncr_base + reg);
+}
+
+static int sd_pcie_get_clk(struct sd_pcie *sd_pcie,
+			       struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+
+	sd_pcie->pcie_aclk = devm_clk_get(dev, "pcie_aclk");
+	if (IS_ERR(sd_pcie->pcie_aclk))
+		return PTR_ERR(sd_pcie->pcie_aclk);
+
+	sd_pcie->pcie_pclk = devm_clk_get(dev, "pcie_pclk");
+	if (IS_ERR(sd_pcie->pcie_pclk))
+		return PTR_ERR(sd_pcie->pcie_pclk);
+
+	sd_pcie->phy_ref = devm_clk_get(dev, "pcie_phy_ref");
+	if (IS_ERR(sd_pcie->phy_ref))
+		return PTR_ERR(sd_pcie->phy_ref);
+
+	sd_pcie->phy_pclk = devm_clk_get(dev, "pcie_phy_pclk");
+	if (IS_ERR(sd_pcie->phy_pclk))
+		return PTR_ERR(sd_pcie->phy_pclk);
+
+	return 0;
+}
+
+static int sd_pcie_get_resource(struct sd_pcie *sd_pcie,
+				    struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct resource *ctrl_ncr;
+	struct resource *phy;
+	struct resource *phy_ncr;
+	struct resource *dbi;
+
+	ctrl_ncr = platform_get_resource_byname(pdev, IORESOURCE_MEM,
+				      "ctrl_ncr");
+	sd_pcie->ctrl_ncr_base = devm_ioremap_resource(dev, ctrl_ncr);
+	if (IS_ERR(sd_pcie->ctrl_ncr_base))
+		return PTR_ERR(sd_pcie->ctrl_ncr_base);
+
+	phy = platform_get_resource_byname(pdev, IORESOURCE_MEM, "phy");
+	sd_pcie->phy_base = devm_ioremap_resource(dev, phy);
+	if (IS_ERR(sd_pcie->phy_base))
+		return PTR_ERR(sd_pcie->phy_base);
+
+	dbi = platform_get_resource_byname(pdev, IORESOURCE_MEM, "dbi");
+	sd_pcie->pcie->dbi_base = devm_ioremap_resource(dev, dbi);
+	if (IS_ERR(sd_pcie->pcie->dbi_base))
+		return PTR_ERR(sd_pcie->pcie->dbi_base);
+
+	sd_pcie->pcie->atu_base =
+	    sd_pcie->pcie->dbi_base + SD_PCIE_ATU_OFFSET;
+
+	phy_ncr = platform_get_resource_byname(pdev, IORESOURCE_MEM, "phy_ncr");
+	sd_pcie->phy_ncr_base = devm_ioremap_resource(dev, phy_ncr);
+	if (IS_ERR(sd_pcie->phy_ncr_base))
+		return PTR_ERR(sd_pcie->phy_ncr_base);
+
+	return 0;
+}
+
+void sd_pcie_phy_refclk_sel(struct sd_pcie *sd_pcie)
+{
+	u32 reg_val;
+
+	u32 flag = sd_pcie->phy_refclk_sel & PHY_REFCLK_USE_MASK;
+	bool diffbuf_out_en = !!(sd_pcie->phy_refclk_sel >> 31);
+
+	if ((flag == PHY_REFCLK_USE_DIFFBUF) && diffbuf_out_en) {
+		pr_info("[ERROR] wrong ref clk cfg\n");
+		BUG();
+	}
+
+	if (flag == PHY_REFCLK_USE_INTERNAL) {
+		pr_info("using internel ref clk\n");
+		reg_val = sd_phy_ncr_readl(sd_pcie, PCIE_PHY_NCR_CTRL1);
+		reg_val &= ~PHY_REF_USE_PAD_BIT;
+		reg_val &= ~PHY_REF_ALT_CLK_SEL_BIT;
+		sd_phy_ncr_writel(sd_pcie, reg_val, PCIE_PHY_NCR_CTRL1);
+	} else if (flag == PHY_REFCLK_USE_EXTERNAL) {
+		pr_info("using externel ref clk\n");
+		reg_val = sd_phy_ncr_readl(sd_pcie, PCIE_PHY_NCR_CTRL1);
+		reg_val |= PHY_REF_USE_PAD_BIT;
+		sd_phy_ncr_writel(sd_pcie, reg_val, PCIE_PHY_NCR_CTRL1);
+	} else if (flag == PHY_REFCLK_USE_DIFFBUF) {
+		pr_info("using diffbuf_extref_clk\n");
+		reg_val = sd_phy_ncr_readl(sd_pcie, PCIE_PHY_NCR_CTRL1);
+		reg_val &= ~(PHY_REF_USE_PAD_BIT);
+		reg_val |= PHY_REF_ALT_CLK_SEL_BIT;
+		sd_phy_ncr_writel(sd_pcie, reg_val, PCIE_PHY_NCR_CTRL1);
+
+		reg_val = sd_phy_ncr_readl(sd_pcie,
+				       PCIE_PHY_NCR_CTRL15);
+		reg_val &= ~0x1;
+		sd_phy_ncr_writel(sd_pcie, reg_val,
+				       PCIE_PHY_NCR_CTRL15);
+	} else {
+		pr_info("error phy_refclk_sel\n");
+		BUG();
+	}
+
+	if (diffbuf_out_en) {
+		pr_info("diffbuf out enable\n");
+		reg_val = sd_phy_ncr_readl(sd_pcie,
+				      PCIE_PHY_NCR_CTRL15);
+		reg_val |= 0x1;
+		sd_phy_ncr_writel(sd_pcie, reg_val,
+				      PCIE_PHY_NCR_CTRL15);
+	}
+}
+
+static void sd_pcie_lane_config(struct sd_pcie *sd_pcie)
+{
+	u32 lane0 = GET_LANE0_CFG(sd_pcie->lane_config);
+	u32 lane1 = GET_LANE1_CFG(sd_pcie->lane_config);
+
+	u32 reg_val = 0;
+
+	reg_val = sd_phy_ncr_readl(sd_pcie, PCIE_PHY_NCR_CTRL0);
+	if (lane1 == PCIE2_INDEX)
+		reg_val |= BIF_EN_BIT;
+	else
+		reg_val &= ~BIF_EN_BIT;
+	sd_phy_ncr_writel(sd_pcie, reg_val, PCIE_PHY_NCR_CTRL0);
+
+	reg_val = 0;
+
+	if (lane0 == NO_PCIE)
+		reg_val |= 0xf2;
+	if (lane1 == NO_PCIE)
+		reg_val |= 0xf20000;
+	sd_phy_ncr_writel(sd_pcie, reg_val, PCIE_PHY_NCR_CTRL25);
+}
+
+static int sd_pcie_phy_init(struct sd_pcie *sd_pcie)
+{
+	u32 reg_val;
+
+	sd_pcie_phy_refclk_sel(sd_pcie);
+
+	reg_val = sd_phy_ncr_readl(sd_pcie, PCIE_PHY_NCR_CTRL0);
+	reg_val |= CR_CKEN_BIT;
+	sd_phy_ncr_writel(sd_pcie, reg_val, PCIE_PHY_NCR_CTRL0);
+
+	reg_val = sd_phy_ncr_readl(sd_pcie, PCIE_PHY_NCR_CTRL4);
+	reg_val &= ~CR_ADDR_MODE_BIT;
+	sd_phy_ncr_writel(sd_pcie, reg_val, PCIE_PHY_NCR_CTRL4);
+
+	sd_pcie_lane_config(sd_pcie);
+
+	reg_val = sd_phy_ncr_readl(sd_pcie, PCIE_PHY_NCR_CTRL0);
+	reg_val &= ~PHY_RESET_BIT;
+	sd_phy_ncr_writel(sd_pcie, reg_val, PCIE_PHY_NCR_CTRL0);
+
+	usleep_range(TIME_PHY_RST_MIN, TIME_PHY_RST_MAX);
+
+	return 0;
+}
+
+static int sd_pcie_clk_ctrl(struct sd_pcie *sd_pcie, bool enable)
+{
+	int ret = 0;
+
+	if (!enable)
+		goto disable_clk;
+
+	ret = clk_prepare_enable(sd_pcie->phy_ref);
+	if (ret)
+		return ret;
+
+	ret = clk_prepare_enable(sd_pcie->phy_pclk);
+	if (ret)
+		goto phy_pclk_fail;
+
+	ret = clk_prepare_enable(sd_pcie->pcie_aclk);
+	if (ret)
+		goto pcie_aclk_fail;
+
+	ret = clk_prepare_enable(sd_pcie->pcie_pclk);
+	if (ret)
+		goto pcie_pclk_fail;
+
+	return 0;
+
+disable_clk:
+	clk_disable_unprepare(sd_pcie->pcie_pclk);
+
+pcie_pclk_fail:
+	clk_disable_unprepare(sd_pcie->pcie_aclk);
+
+pcie_aclk_fail:
+	clk_disable_unprepare(sd_pcie->phy_pclk);
+
+phy_pclk_fail:
+	clk_disable_unprepare(sd_pcie->phy_ref);
+
+	return ret;
+}
+
+static int sd_pcie_power_on(struct sd_pcie *sd_pcie)
+{
+	int ret;
+
+	ret = sd_pcie_clk_ctrl(sd_pcie, true);
+	if (ret)
+		return ret;
+
+	sd_pcie_phy_init(sd_pcie);
+
+	return 0;
+}
+
+static int sd_pcie_link_up(struct dw_pcie *pcie)
+{
+	struct sd_pcie *sd_pcie = to_sd_pcie(pcie);
+
+	u32 reg_val = sd_ctrl_ncr_readl(sd_pcie, PCIE_CTRL_NCR_INTR0);
+
+	if ((reg_val & INTR_SMLH_LINK_UP) != INTR_SMLH_LINK_UP)
+		return 0;
+
+	if ((reg_val & INTR_RDLH_LINK_UP) != INTR_RDLH_LINK_UP)
+		return 0;
+
+	return 1;
+}
+
+static void sd_pcie_core_init(struct sd_pcie *sd_pcie)
+{
+	u32 reg_val;
+
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN0);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN1);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN2);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN3);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN4);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN5);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN6);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN7);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN8);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN9);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN10);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN11);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_INTEN12);
+
+	reg_val = sd_ctrl_ncr_readl(sd_pcie, PCIE_CTRL_NCR_CTRL0);
+	reg_val &= ~APP_HOLD_PHY_RST_BIT;
+	sd_ctrl_ncr_writel(sd_pcie, reg_val, PCIE_CTRL_NCR_CTRL0);
+
+	reg_val = sd_ctrl_ncr_readl(sd_pcie, PCIE_CTRL_NCR_CTRL0);
+	reg_val |= DEVICE_TYPE_BIT;
+	sd_ctrl_ncr_writel(sd_pcie, reg_val, PCIE_CTRL_NCR_CTRL0);
+
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_CTRL21);
+	sd_ctrl_ncr_writel(sd_pcie, 0, PCIE_CTRL_NCR_CTRL23);
+}
+
+static int sd_pcie_establish_link(struct pcie_port *pp)
+{
+	struct dw_pcie *pcie = to_dw_pcie_from_pp(pp);
+	struct sd_pcie *sd_pcie = to_sd_pcie(pcie);
+	struct device *dev = sd_pcie->pcie->dev;
+	u32 reg_val;
+	int count = 0;
+
+	if (sd_pcie_link_up(pcie))
+		return 0;
+
+	dw_pcie_setup_rc(pp);
+
+	reg_val = sd_ctrl_ncr_readl(sd_pcie, PCIE_CTRL_NCR_CTRL0);
+	reg_val |= APP_LTSSM_EN_BIT;
+	sd_ctrl_ncr_writel(sd_pcie, reg_val, PCIE_CTRL_NCR_CTRL0);
+
+	/* check if the link is up or not */
+	while (!sd_pcie_link_up(pcie)) {
+		usleep_range(LINK_WAIT_MIN, LINK_WAIT_MAX);
+		count++;
+		if (count == 1000) {
+			dev_err(dev, "Link Fail\n");
+			return -EINVAL;
+		}
+	}
+
+	return 0;
+}
+
+static int sd_pcie_host_init(struct pcie_port *pp)
+{
+	struct dw_pcie *pcie = to_dw_pcie_from_pp(pp);
+	struct sd_pcie *sd_pcie = to_sd_pcie(pcie);
+
+	sd_pcie_core_init(sd_pcie);
+
+	sd_pcie_establish_link(pp);
+
+	if (IS_ENABLED(CONFIG_PCI_MSI))
+		dw_pcie_msi_init(pp);
+
+	return 0;
+}
+
+static struct dw_pcie_ops sd_dw_pcie_ops = {
+	.link_up = sd_pcie_link_up,
+};
+
+static const struct dw_pcie_host_ops sd_pcie_host_ops = {
+	.host_init = sd_pcie_host_init,
+};
+
+static irqreturn_t sd_pcie_msi_handler(int irq, void *arg)
+{
+	struct sd_pcie *sd_pcie = arg;
+	struct dw_pcie *pcie = sd_pcie->pcie;
+	struct pcie_port *pp = &pcie->pp;
+
+	return dw_handle_msi_irq(pp);
+}
+
+static int __init sd_add_pcie_port(struct dw_pcie *pcie,
+				       struct platform_device *pdev)
+{
+	int ret;
+	struct device *dev = &pdev->dev;
+	struct pcie_port *pp = &(pcie->pp);
+	struct sd_pcie *sd_pcie = to_sd_pcie(pcie);
+
+	if (IS_ENABLED(CONFIG_PCI_MSI)) {
+		pp->msi_irq = platform_get_irq_byname(pdev, "msi");
+		if (pp->msi_irq <= 0) {
+			dev_err(dev, "failed to get MSI irq\n");
+			return -ENODEV;
+		}
+
+		ret = devm_request_irq(dev, pp->msi_irq,
+				     sd_pcie_msi_handler,
+				     IRQF_SHARED | IRQF_NO_THREAD,
+				     "sd-pcie-msi", sd_pcie);
+		if (ret) {
+			dev_err(dev, "failed to request MSI irq\n");
+			return ret;
+		}
+	}
+
+	pp->ops = &sd_pcie_host_ops;
+
+	ret = dw_pcie_host_init(pp);
+	if (ret) {
+		dev_err(dev, "failed to initialize host\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int sd_pcie_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct sd_pcie *sd_pcie;
+	struct dw_pcie *pcie;
+	int ret;
+
+	if (!dev->of_node) {
+		dev_err(dev, "NULL node\n");
+		return -EINVAL;
+	}
+
+	sd_pcie = devm_kzalloc(dev, sizeof(struct sd_pcie), GFP_KERNEL);
+	if (!sd_pcie)
+		return -ENOMEM;
+
+	pcie = devm_kzalloc(dev, sizeof(*pcie), GFP_KERNEL);
+	if (!pcie)
+		return -ENOMEM;
+
+	pcie->dev = dev;
+	pcie->ops = &sd_dw_pcie_ops;
+	sd_pcie->pcie = pcie;
+	sd_pcie->lane_config = PCIE1_2_EACH_ONE_LANE;
+	sd_pcie->phy_refclk_sel = PHY_REFCLK_USE_EXTERNAL | DIFFBUF_OUT_EN;
+
+	ret = sd_pcie_get_clk(sd_pcie, pdev);
+	if (ret)
+		return ret;
+
+	ret = sd_pcie_get_resource(sd_pcie, pdev);
+	if (ret)
+		return ret;
+
+	ret = sd_pcie_power_on(sd_pcie);
+	if (ret)
+		return ret;
+
+	platform_set_drvdata(pdev, sd_pcie);
+
+	ret = sd_add_pcie_port(sd_pcie->pcie, pdev);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static const struct of_device_id sd_pcie_match[] = {
+	{.compatible = "semidrive,kunlun-pcie"},
+	{},
+};
+
+static struct platform_driver sd_pcie_driver = {
+	.probe = sd_pcie_probe,
+	.driver = {
+		.name = "sd-pcie",
+		.of_match_table = sd_pcie_match,
+		.suppress_bind_attrs = true,
+	},
+};
+builtin_platform_driver(sd_pcie_driver);
