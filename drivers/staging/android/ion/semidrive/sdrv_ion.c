65a64caa3a9a127d64b99dca16033edc23853aa6
kan.qiu <kan.qiu@semidrive.com>
2020-05-12 15:44:44 +0800

    Add Ion-advanced module
    Except the system defaults of Ion, Semidrive can introduce other types
    of Ion heap now, which can be customized for such as module-specific
    statical memory regions.
    
    Currently, the new supported heap is only carveout.
    
    Change-Id: I8d9c788fbb6ee4052b1b3af9d4384a2e914e48d5

---
+++ drivers/staging/android/ion/semidrive/sdrv_ion.c
@@ -0,0 +1,249 @@
+/*
+* Copyright (c) 2019 Semidrive Semiconductor.
+* All rights reserved.
+*
+*/
+
+#include "sdrv_ion.h"
+
+#define SEMIDRIVE_COMPAT_STR	"semidrive,sdrv-ion"
+
+#define MAKE_HEAP_TYPE_MAPPING(h) { .name = ION_HEAP_NAME_##h, \
+			.heap_type = ION_HEAP_TYPE_##h, }
+
+static struct heap_types_info {
+	const char *name;
+	enum ion_heap_type heap_type;
+} heap_types_info[] = {
+	MAKE_HEAP_TYPE_MAPPING(CARVEOUT),
+	//MAKE_HEAP_TYPE_MAPPING(SECURE_DMA),
+	//MAKE_HEAP_TYPE_MAPPING(HYP_CMA),
+};
+
+static struct ion_heap *sdrv_ion_heap_create(struct ion_platform_heap *heap_data)
+{
+	struct ion_heap *heap = NULL;
+	switch ((int)heap_data->type) {
+	case ION_HEAP_TYPE_CARVEOUT:
+		heap = ion_carveout_heap_create(heap_data);
+		break;
+	//case ION_HEAP_TYPE_SECURE_DMA:
+	//	break;
+	//case ION_HEAP_TYPE_HYP_CMA:
+	//	break;
+	default:
+		pr_err("%s: Invalid heap type %d\n", __func__,
+		       heap_data->type);
+		return ERR_PTR(-EINVAL);
+	}
+
+	if (IS_ERR_OR_NULL(heap)) {
+		pr_err("%s: error creating heap %s type %d base %pa size %zu\n",
+		       __func__, heap_data->name, heap_data->type,
+		       heap_data->base, heap_data->size);
+		return ERR_PTR(-EINVAL);
+	}
+
+	heap->name = heap_data->name;
+
+	return heap;
+}
+
+static int sdrv_ion_get_heap_type_from_dt_node(struct device_node *node,
+					struct ion_platform_heap *heap)
+{
+	const char *type_name;
+	int i, ret = -EINVAL;
+	ret = of_property_read_string(node, "sdrv,ion-heap-type", &type_name);
+	if (ret)
+		goto out;
+	for (i = 0; i < ARRAY_SIZE(heap_types_info); ++i) {
+		if (!strcmp(heap_types_info[i].name, type_name)) {
+			heap->type = heap_types_info[i].heap_type;
+			ret = 0;
+			goto out;
+		}
+	}
+	WARN(1, "Unknown heap type: %s for heap %s. You might need to update heap_types_info in %s",
+		type_name, heap->name, __FILE__);
+out:
+	return ret;
+}
+
+static int sdrv_ion_get_heap_memory_region(struct device_node *node,
+					struct ion_platform_heap *heap)
+{
+	struct device_node *target;
+	const __be32 *basep;
+	u64 size, base;
+
+	target = of_parse_phandle(node, "memory-region", 0);
+	if (!target) {
+		WARN(1, "Not found node of memory-region for heap %s\n", heap->name);
+		goto err;
+	}
+	basep = of_get_address(target, 0, &size, NULL);
+	if (!basep) {
+		WARN(1, "Not found address from reg property");
+		goto err;
+	} else {
+		heap->size = size;
+		base = of_translate_address(target, basep);
+		if (OF_BAD_ADDR == heap->base) {
+			WARN(1, "Failed to parse DT node for heap %s\n", heap->name);
+			goto err;
+		}
+	}
+
+	heap->base = base;
+
+	of_node_put(target);
+
+	return 0;
+
+err:
+	return -EINVAL;
+}
+
+static struct ion_platform_heap *sdrv_ion_parse_dt(struct platform_device *pdev,
+					uint32_t *num_valid, uint32_t *num_child)
+{
+	struct ion_platform_heap *heaps = NULL;
+	uint32_t num_heaps = 0;
+	int idx = 0;
+	const struct device_node *dt_node = pdev->dev.of_node;
+	struct device_node *node;
+        int ret = 0;
+
+	for_each_available_child_of_node(dt_node, node)
+		num_heaps++;
+
+	if (0 == num_heaps)
+		return ERR_PTR(-EINVAL);
+
+	heaps = kzalloc(sizeof(struct ion_platform_heap) * num_heaps, GFP_KERNEL);
+	if (!heaps) {
+		return ERR_PTR(-ENOMEM);
+	}
+
+	*num_child = num_heaps;
+
+	for_each_available_child_of_node(dt_node, node) {
+		ret = sdrv_ion_get_heap_type_from_dt_node(node, &heaps[idx]);
+		if (ret)
+			continue;
+		ret = sdrv_ion_get_heap_memory_region(node, &heaps[idx]);
+		if (ret)
+			continue;
+
+		heaps[idx].name = node->name;
+		pr_info("Scan out new heap %s\n", heaps[idx].name);
+		++idx;
+	}
+
+	if (0 == idx)
+		goto free_heaps;
+
+	*num_valid = idx;
+	return heaps;
+
+free_heaps:
+	kfree(heaps);
+	return ERR_PTR(-EINVAL);
+}
+
+static int sdrv_ion_probe(struct platform_device *pdev)
+{
+	struct ion_heap **heaps;
+	static struct sdrv_ion_device_data dev_data;
+	struct ion_platform_heap *pdatas;
+	uint32_t num_valid, num_child;
+	int ret = 0;
+	int i;
+
+	pdatas = sdrv_ion_parse_dt(pdev, &num_valid, &num_child);
+	if (IS_ERR(pdatas)) {
+		ret = PTR_ERR(pdatas);
+		goto out;
+	}
+
+	heaps = kcalloc(num_valid, sizeof(struct ion_heap *), GFP_KERNEL);
+	if (!heaps) {
+		ret = -ENOMEM;
+		goto freepdatas;
+	}
+
+	dev_data.num_heaps = num_valid;
+	dev_data.heaps = heaps;
+
+	/* create the heaps as specified in the board dts file */
+	for (i = 0; i < num_valid; i++) {
+		struct ion_platform_heap *heap_data = &pdatas[i];
+		heaps[i] = sdrv_ion_heap_create(heap_data);
+		if (IS_ERR_OR_NULL(heaps[i])) {
+			heaps[i] = NULL;
+			continue;
+		} else {
+			if (heap_data->size)
+				pr_info("ION heap %s created at %pa with size %zx\n",
+					heap_data->name,
+					heap_data->base,
+					heap_data->size);
+			else
+				pr_info("ION heap %s created\n",
+					heap_data->name);
+		}
+
+		ion_device_add_heap(heaps[i]);
+	}
+
+	platform_set_drvdata(pdev, heaps);
+
+freepdatas:
+	kfree(pdatas);
+
+out:
+	return ret;
+}
+
+static int sdrv_ion_remove(struct platform_device *pdev)
+{
+	struct sdrv_ion_device_data *idev = platform_get_drvdata(pdev);
+	int i;
+
+	for (i = 0; i < idev->num_heaps; i++)
+		kfree(idev->heaps[i]);
+
+	/* Todo: can release additional resources like debugfs */
+	kfree(idev->heaps);
+
+	kfree(idev);
+	return 0;
+}
+
+static struct of_device_id sdrv_ion_match_table[] = {
+	{.compatible = SEMIDRIVE_COMPAT_STR},
+	{},
+};
+
+static struct platform_driver sdrv_ion_driver = {
+	.probe = sdrv_ion_probe,
+	.remove = sdrv_ion_remove,
+	.driver = {
+		.name = "ion-sdrv",
+		.of_match_table = sdrv_ion_match_table,
+	},
+};
+
+static int __init sdrv_ion_init(void)
+{
+	return platform_driver_register(&sdrv_ion_driver);
+}
+
+static void __exit sdrv_ion_exit(void)
+{
+	platform_driver_unregister(&sdrv_ion_driver);
+}
+
+subsys_initcall(sdrv_ion_init);
+module_exit(sdrv_ion_exit);
