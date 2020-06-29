4466a13b0a2036a8a558be7732113f8643a66509
ye.liu <ye.liu@semidrive.com>
2020-06-29 15:38:51 +0800

    dts: use dt-binding files for rpmsg virtio memory range
    Change-Id: I84203cfe89b1553604215b37bc34e76e76f45a48

---
+++ include/dt-bindings/rpmsg/sd,x9h-default.h
@@ -0,0 +1,23 @@
+ /*
+ *
+ * Copyright(c) 2020 Semidrive
+ * *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __SDRV_SHM_X9_DEFAULT_H__
+#define __SDRV_SHM_X9_DEFAULT_H__
+
+#define RPMSG0_MEM_HEAD_BASE	0x42900000
+#define RPMSG0_MEM_HEAD_SIZE	0x10000
+#define RPMSG0_MEM_POOL_BASE    0x42910000
+#define RPMSG0_MEM_POOL_SIZE	0xf0000
+
+#define RPMSG1_MEM_HEAD_BASE	0x42b00000
+#define RPMSG1_MEM_HEAD_SIZE	0x10000
+#define RPMSG1_MEM_POOL_BASE    0x42b10000
+#define RPMSG1_MEM_POOL_SIZE	0xf0000
+
+#endif //__SDRV_SHM_X9_DEFAULT_H__
