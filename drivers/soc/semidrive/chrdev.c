a8e6861cc4d05a61efe128566f5112137415f67a
ye.liu <ye.liu@semidrive.com>
2020-08-28 10:10:48 +0800

    add dcf user to support multi-instance device
    Change-Id: I2e465a9bb7000d246aa516ccd0b301a50d60e343

--- drivers/soc/semidrive/chrdev.c
+++ drivers/soc/semidrive/chrdev.c
@@ -27,8 +27,10 @@
 
 #include <linux/soc/semidrive/ipcc.h>
 
+#include "uapi/dcf-ioctl.h"
+
 union dcf_ioctl_arg {
-	int a;
+	struct dcf_usercontext	context;
 	int b;
 };
 
@@ -43,10 +45,20 @@ struct dcf_device {
 	struct device *dev;
 	struct rpmsg_device *rpdev;
 	struct mutex ept_lock;
+	struct list_head users;
+	atomic_t user_count;
+};
 
+struct dcf_user {
+	struct list_head list_node;
+	struct dcf_device *eptdev;
+	u8 user_id;
+	u8 user_id_other;
 	spinlock_t queue_lock;
 	struct sk_buff_head queue;
 	wait_queue_head_t readq;
+	int pid;
+
 };
 
 static int validate_ioctl_arg(unsigned int cmd, union dcf_ioctl_arg *arg)
@@ -54,7 +66,7 @@ static int validate_ioctl_arg(unsigned int cmd, union dcf_ioctl_arg *arg)
 	int ret = 0;
 
 	switch (cmd) {
-	case 1:
+	case DCF_IOC_SET_USER_CTX:
 
 		break;
 	default:
@@ -67,17 +79,15 @@ static int validate_ioctl_arg(unsigned int cmd, union dcf_ioctl_arg *arg)
 /* fix up the cases where the ioctl direction bits are incorrect */
 static unsigned int dcf_ioctl_dir(unsigned int cmd)
 {
-	switch (cmd) {
-	default:
-		return _IOC_DIR(cmd);
-	}
+	return _IOC_DIR(cmd);
 }
 
-long ioctl_dcf(struct file *filp, unsigned int cmd, unsigned long arg)
+long dcf_dev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-	int ret = 0;
-	unsigned int dir;
+	struct dcf_user *user = filp->private_data;
 	union dcf_ioctl_arg data;
+	unsigned int dir;
+	int ret = 0;
 
 	dir = dcf_ioctl_dir(cmd);
 
@@ -102,13 +112,20 @@ long ioctl_dcf(struct file *filp, unsigned int cmd, unsigned long arg)
 		memset(&data, 0, sizeof(data));
 
 	switch (cmd) {
-	case 1:
+	case DCF_IOC_SET_USER_CTX:
 	{
-		break;
-	}
-	case 2:
+		struct dcf_usercontext *ctx = &data.context;
+		if (ctx->user_id >= DCF_USER_ANY)
+			return -ERANGE;
+
+		if (user->user_id != DCF_USER_ANY)
+			return -EEXIST;
+
+		user->user_id = ctx->user_id;
+		user->user_id_other = ctx->user_id_other;
 
 		break;
+	}
 	default:
 		return -ENOTTY;
 	}
@@ -120,10 +137,11 @@ long ioctl_dcf(struct file *filp, unsigned int cmd, unsigned long arg)
 	return ret;
 }
 
-static ssize_t read_dcf(struct file *filp, char __user *buf,
+static ssize_t dcf_dev_read(struct file *filp, char __user *buf,
 				 size_t len, loff_t *f_pos)
 {
-	struct dcf_device *eptdev = filp->private_data;
+	struct dcf_user *user = filp->private_data;
+	struct dcf_device *eptdev = user->eptdev;
 	struct rpmsg_device *rpdev = eptdev->rpdev;
 	unsigned long flags;
 	struct sk_buff *skb;
@@ -132,18 +150,18 @@ static ssize_t read_dcf(struct file *filp, char __user *buf,
 	if (!rpdev->ept)
 		return -EPIPE;
 
-	spin_lock_irqsave(&eptdev->queue_lock, flags);
+	spin_lock_irqsave(&user->queue_lock, flags);
 
 	/* Wait for data in the queue */
-	if (skb_queue_empty(&eptdev->queue)) {
-		spin_unlock_irqrestore(&eptdev->queue_lock, flags);
+	if (skb_queue_empty(&user->queue)) {
+		spin_unlock_irqrestore(&user->queue_lock, flags);
 
 		if (filp->f_flags & O_NONBLOCK)
 			return -EAGAIN;
 
 		/* Wait until we get data or the endpoint goes away */
-		if (wait_event_interruptible(eptdev->readq,
-						 !skb_queue_empty(&eptdev->queue) ||
+		if (wait_event_interruptible(user->readq,
+						 !skb_queue_empty(&user->queue) ||
 						 !rpdev->ept))
 			return -ERESTARTSYS;
 
@@ -151,11 +169,11 @@ static ssize_t read_dcf(struct file *filp, char __user *buf,
 		if (!rpdev->ept)
 			return -EPIPE;
 
-		spin_lock_irqsave(&eptdev->queue_lock, flags);
+		spin_lock_irqsave(&user->queue_lock, flags);
 	}
 
-	skb = skb_dequeue(&eptdev->queue);
-	spin_unlock_irqrestore(&eptdev->queue_lock, flags);
+	skb = skb_dequeue(&user->queue);
+	spin_unlock_irqrestore(&user->queue_lock, flags);
 	if (!skb)
 		return -EFAULT;
 
@@ -168,11 +186,11 @@ static ssize_t read_dcf(struct file *filp, char __user *buf,
 	return use;
 }
 
-
-static ssize_t write_dcf(struct file *filp, const char __user *buf,
+static ssize_t dcf_dev_write(struct file *filp, const char __user *buf,
 				  size_t len, loff_t *f_pos)
 {
-	struct dcf_device *eptdev = filp->private_data;
+	struct dcf_user *user = filp->private_data;
+	struct dcf_device *eptdev = user->eptdev;
 	struct rpmsg_device *rpdev = eptdev->rpdev;
 	void *kbuf;
 	int ret;
@@ -204,18 +222,19 @@ free_kbuf:
 	return ret < 0 ? ret : len;
 }
 
-static unsigned int poll_dcf(struct file *filp, poll_table *wait)
+static unsigned int dcf_dev_poll(struct file *filp, poll_table *wait)
 {
-	struct dcf_device *eptdev = filp->private_data;
+	struct dcf_user *user = filp->private_data;
+	struct dcf_device *eptdev = user->eptdev;
 	struct rpmsg_device *rpdev = eptdev->rpdev;
 	unsigned int mask = 0;
 
 	if (!rpdev->ept)
 		return POLLERR;
 
-	poll_wait(filp, &eptdev->readq, wait);
+	poll_wait(filp, &user->readq, wait);
 
-	if (!skb_queue_empty(&eptdev->queue))
+	if (!skb_queue_empty(&user->queue))
 		mask |= POLLIN | POLLRDNORM;
 
 	mask |= rpmsg_poll(rpdev->ept, filp, wait);
@@ -227,7 +246,13 @@ static int dcf_eptdev_cb(struct rpmsg_device *rpdev, void *buf, int len,
 			void *priv, u32 addr)
 {
 	struct dcf_device *eptdev = priv;
+	struct dcf_user *user;
 	struct sk_buff *skb;
+
+	/* No recepient any more, just drop this message */
+	if (!atomic_read(&eptdev->user_count)) {
+		return 0;
+	}
 
 	skb = alloc_skb(len, GFP_ATOMIC);
 	if (!skb)
@@ -235,115 +260,151 @@ static int dcf_eptdev_cb(struct rpmsg_device *rpdev, void *buf, int len,
 
 	skb_put_data(skb, buf, len);
 
-	spin_lock(&eptdev->queue_lock);
-	skb_queue_tail(&eptdev->queue, skb);
-	spin_unlock(&eptdev->queue_lock);
+	/*TODO: dispatch to a dcf_user according to src/dst address */
+	list_for_each_entry(user, &eptdev->users, list_node) {
+		spin_lock(&user->queue_lock);
+		skb_queue_tail(&user->queue, skb);
+		spin_unlock(&user->queue_lock);
 
-	/* wake up any blocking processes, waiting for new data */
-	wake_up_interruptible(&eptdev->readq);
+		/* wake up any blocking processes, waiting for new data */
+		wake_up_interruptible(&user->readq);
+
+		break;
+	}
 
 	return 0;
 }
 
-static int open_dcf(struct inode *inode, struct file *filp)
+static int dcf_dev_open(struct inode *inode, struct file *filp)
 {
 	struct dcf_device *eptdev = filp->private_data;
 	struct rpmsg_device *rpdev = eptdev->rpdev;
 	struct device *dev = eptdev->dev;
 	struct rpmsg_channel_info chinfo;
+	struct dcf_user *user;
 
 	get_device(dev);
-	mutex_init(&eptdev->ept_lock);
-	spin_lock_init(&eptdev->queue_lock);
-	skb_queue_head_init(&eptdev->queue);
-	init_waitqueue_head(&eptdev->readq);
 
-	if (rpdev->ept) {
-		dev_err(dev, "Already exist\n");
-		return -EEXIST;
+	if (!atomic_read(&eptdev->user_count)) {
+		mutex_init(&eptdev->ept_lock);
+		INIT_LIST_HEAD(&eptdev->users);
+
+		if (rpdev->ept) {
+			dev_err(dev, "Already exist\n");
+			return -EEXIST;
+		}
+
+		strncpy(chinfo.name, eptdev->name, RPMSG_NAME_SIZE);
+		chinfo.src = eptdev->myaddr;
+		chinfo.dst = RPMSG_ADDR_ANY;
+
+		rpdev->ept = rpmsg_create_ept(rpdev, dcf_eptdev_cb, eptdev, chinfo);
+		if (!rpdev->ept) {
+			dev_err(dev, "failed to create endpoint\n");
+			put_device(dev);
+			return -ENOMEM;
+		}
+		rpdev->src = rpdev->ept->addr;
+
+		dev_info(dev, "first user, creating ept %s %d->%d \n", chinfo.name, chinfo.src, chinfo.dst);
 	}
 
-	strncpy(chinfo.name, eptdev->name, RPMSG_NAME_SIZE);
-	chinfo.src = eptdev->myaddr;
-	chinfo.dst = RPMSG_ADDR_ANY;
-
-	rpdev->ept = rpmsg_create_ept(rpdev, dcf_eptdev_cb, eptdev, chinfo);
-	if (!rpdev->ept) {
+	user = kzalloc(sizeof(struct dcf_user), GFP_KERNEL);
+	if (!user) {
 		dev_err(dev, "failed to create endpoint\n");
 		put_device(dev);
 		return -ENOMEM;
 	}
-	rpdev->src = rpdev->ept->addr;
 
-	dev_info(dev, "open %s %d->%d \n", chinfo.name, chinfo.src, chinfo.dst);
+	user->user_id = DCF_USER_ANY;	/* Not assigned */
+	user->user_id_other = DCF_USER_ANY;
+	user->eptdev = eptdev;
+	user->pid = current->group_leader->pid;
+	spin_lock_init(&user->queue_lock);
+	skb_queue_head_init(&user->queue);
+	init_waitqueue_head(&user->readq);
+	list_add(&user->list_node, &eptdev->users);
+	filp->private_data = user;
+	atomic_inc(&eptdev->user_count);
+	dev_info(dev, "user=%d pid=%d opened\n", user->user_id, user->pid);
 
 	return 0;
 }
 
-static int release_dcf(struct inode *inode, struct file *filp)
+static int dcf_dev_release(struct inode *inode, struct file *filp)
 {
-	struct dcf_device *eptdev = filp->private_data;
+	struct dcf_user *user = filp->private_data;
+	struct dcf_device *eptdev = user->eptdev;
 	struct rpmsg_device *rpdev = eptdev->rpdev;
 	struct device *dev = eptdev->dev;
 	struct sk_buff *skb;
 
-	/* Close the endpoint, if it's not already destroyed by the parent */
-	mutex_lock(&eptdev->ept_lock);
-	if (rpdev->ept) {
-			rpmsg_destroy_ept(rpdev->ept);
-			rpdev->ept = NULL;
-	}
-	mutex_unlock(&eptdev->ept_lock);
+	list_del(&user->list_node);
 
 	/* Discard all SKBs */
-	while (!skb_queue_empty(&eptdev->queue)) {
-		skb = skb_dequeue(&eptdev->queue);
+	spin_lock(&user->queue_lock);
+	while (!skb_queue_empty(&user->queue)) {
+		skb = skb_dequeue(&user->queue);
 		kfree_skb(skb);
+	}
+	spin_unlock(&user->queue_lock);
+	dev_info(dev, "user=%d pid=%d released!\n", user->user_id, user->pid);
+	kfree(user);
+
+	if (atomic_dec_and_test(&eptdev->user_count)) {
+		/* Close the endpoint, if it's not already destroyed by the parent */
+		mutex_lock(&eptdev->ept_lock);
+		if (rpdev->ept) {
+				rpmsg_destroy_ept(rpdev->ept);
+				rpdev->ept = NULL;
+		}
+		mutex_unlock(&eptdev->ept_lock);
+
+		dev_info(dev, "last user=%d pid=%d released!\n", user->user_id, user->pid);
 	}
 
 	put_device(dev);
-
-	dev_info(dev, "released!\n");
 
 	return 0;
 }
 
 static const struct file_operations __maybe_unused cluster_fops = {
 	.owner   = THIS_MODULE,
-	.read    = read_dcf,
-	.write   = write_dcf,
-	.unlocked_ioctl = ioctl_dcf,
-	.poll    = poll_dcf,
-	.open    = open_dcf,
-	.release = release_dcf,
+	.read    = dcf_dev_read,
+	.write   = dcf_dev_write,
+	.unlocked_ioctl = dcf_dev_ioctl,
+	.poll    = dcf_dev_poll,
+	.open    = dcf_dev_open,
+	.release = dcf_dev_release,
 	.llseek	 = no_llseek,
 };
 
 static const struct file_operations __maybe_unused avm_fops = {
 	.llseek		= no_llseek,
-	.read		= read_dcf,
-	.write		= write_dcf,
-	.unlocked_ioctl = ioctl_dcf,
-	.open		= open_dcf,
-	.release	= release_dcf,
+	.read		= dcf_dev_read,
+	.write		= dcf_dev_write,
+	.unlocked_ioctl = dcf_dev_ioctl,
+	.open		= dcf_dev_open,
+	.release	= dcf_dev_release,
 };
 
 static const struct file_operations __maybe_unused sec_fops = {
 	.llseek		= no_llseek,
-	.read		= read_dcf,
-	.write		= write_dcf,
-	.unlocked_ioctl = ioctl_dcf,
-	.open		= open_dcf,
-	.release	= release_dcf,
+	.read		= dcf_dev_read,
+	.write		= dcf_dev_write,
+	.unlocked_ioctl = dcf_dev_ioctl,
+	.open		= dcf_dev_open,
+	.release	= dcf_dev_release,
 };
 
 static const struct file_operations __maybe_unused vircan_fops = {
 	.llseek		= no_llseek,
-	.read		= read_dcf,
-	.write		= write_dcf,
-	.unlocked_ioctl = ioctl_dcf,
-	.open		= open_dcf,
-	.release	= release_dcf,
+	.read		= dcf_dev_read,
+	.write		= dcf_dev_write,
+	.unlocked_ioctl = dcf_dev_ioctl,
+	.poll		= dcf_dev_poll,
+	.open		= dcf_dev_open,
+	.release	= dcf_dev_release,
 };
 
 static struct dcf_device devlist[] = {
@@ -490,8 +551,8 @@ static void __exit dcf_char_exit(void)
 module_init(dcf_char_init);
 module_exit(dcf_char_exit);
 
-MODULE_AUTHOR("Semidrive Semiconductor");
+MODULE_AUTHOR("<ye.liu@semidrive.com>");
 MODULE_ALIAS("dcf:chrdev");
-MODULE_DESCRIPTION("Semidrive domain-communication-framework driver");
+MODULE_DESCRIPTION("Semidrive DCF User chardev driver");
 MODULE_LICENSE("GPL v2");
 
