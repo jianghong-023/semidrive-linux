4b5b06d8d3c3e96dd204c00324e7c416d7a19d98
dayi.xu <dayi.xu@semidrive.com>
2021-02-05 17:11:52 +0800

    optimize touch framework
    [ID]:Bugzilla – Bug 981
    [Moudle]:touch
    
    Change-Id: Ibd18c518fee4e3d6c44e6f159d913a44cb1172ca

--- drivers/input/touchscreen/semidrive_ts.c
+++ drivers/input/touchscreen/semidrive_ts.c
@@ -29,37 +29,60 @@
 #include <linux/soc/semidrive/ipcc.h>
 #include <linux/soc/semidrive/xenipc.h>
 
+#define STS_MAX_HEIGHT		4096
+#define STS_MAX_WIDTH		4096
+#define STS_CONTACT_SIZE	8
+#define STS_MAX_CONTACTS	10
+#define STS_BUFFER_STATUS_READY BIT(7)
+
+#define STS_VERSION	0xff
+#define STS_ID		0xff
+#define STS_VENDOR	0xff
+
+#define TS_SUPPORT_ANRDOID_MAIN (1<<0)
+#define TS_SUPPORT_ANRDOID_AUX1 (1<<1)
+#define TS_SUPPORT_ANRDOID_AUX2 (1<<2)
+#define TS_SUPPORT_ANRDOID_AUX3 (1<<3)
+
+static struct ts_coord_data {
+	u8 id;
+	u16 x;
+	u16 y;
+	u16 w;
+};
+
+struct ts_report_data {
+	u8 key_value;
+	u8 touch_num;
+	struct ts_coord_data coord_data[10];
+};
+
 struct semidrive_sts_data {
 	struct platform_device *pdev;
 	struct mbox_client client;
 	struct mbox_chan *mbox;
-	struct work_struct rx_work;
 	struct input_dev *input_dev;
+	struct delayed_work ts_work;
+	int ts_work_count;
+	bool read_version_stat;
+	bool read_config_stat;
 	int abs_x_max;
 	int abs_y_max;
 	int x_offset;
 	int y_offset;
-	bool swapped_x_y;
-	bool inverted_x;
-	bool inverted_y;
-	unsigned int max_touch_num;
+	u8 swapped_x_y;
+	u8 inverted_x;
+	u8 inverted_y;
+	u8 max_touch_num;
 	u16 id;
 	u16 version;
-	u32 dev_type;	/* 0: main device, 1: aux device */
+	u16 vendor;
 	u16 instance;
 };
-
-#define STS_MAX_HEIGHT		800
-#define STS_MAX_WIDTH		1280
-#define STS_CONTACT_SIZE		8
-#define STS_MAX_CONTACTS		10
-#define STS_BUFFER_STATUS_READY		BIT(7)
 
 enum sts_op_type {
 	STS_OP_GET_VERSION,
 	STS_OP_GET_CONFIG,
-	STS_OP_SET_CONFIG,
-	STS_OP_RESET,
 	STS_OP_SET_INITED,
 };
 
@@ -85,98 +108,59 @@ struct sts_ioctl_result {
 		struct {
 			u16 version;
 			u16 id;
-			u16 dev_type;
+			u16 vendor;
 		} v;
 		/** used for get_config */
 		struct {
 			u16 abs_x_max;
 			u16 abs_y_max;
-			u16 touch_num;
 			u16 x_offset;
 			u16 y_offset;
+			u8 swapped_x_y;
+			u8 inverted_x;
+			u8 inverted_y;
+			u8 max_touch_num;
 		} gc;
 		u8 data[12];
 	} msg;
 };
 
-static int semidrive_read_input_report(struct semidrive_sts_data *ts, u8 *data)
-{
-	int touch_num;
-
-	if (data[0] & STS_BUFFER_STATUS_READY) {
-		touch_num = data[0] & 0x0f;
-		if (touch_num > ts->max_touch_num) {
-			return -EPROTO;
-		}
-
-		return touch_num;
-	}
-
-	return 0;
-}
-
-static void
-semidrive_sts_report_touch(struct semidrive_sts_data *ts, u8 *coor_data)
-{
-	int id = coor_data[0] & 0x0F;
-	int input_x = get_unaligned_le16(&coor_data[1]);
-	int input_y = get_unaligned_le16(&coor_data[3]);
-	int input_w = get_unaligned_le16(&coor_data[5]);
-
-	//dev_err(&ts->pdev->dev, "(%d %d)\n", input_x, input_y);
-	input_x -= ts->x_offset;
-	input_y -= ts->y_offset;
-	if ((input_x < 0) || (input_x > ts->abs_x_max))
-		return;
-	if ((input_y < 0) || (input_y > ts->abs_y_max))
-		return;
-
-	/* Inversions have to happen before axis swapping */
-	if (ts->inverted_x)
-		input_x = ts->abs_x_max - input_x;
-	if (ts->inverted_y)
-		input_y = ts->abs_y_max - input_y;
-	if (ts->swapped_x_y)
-		swap(input_x, input_y);
-
-	input_mt_slot(ts->input_dev, id);
-	input_mt_report_slot_state(ts->input_dev, MT_TOOL_FINGER, true);
-	input_report_abs(ts->input_dev, ABS_MT_POSITION_X, input_x);
-	input_report_abs(ts->input_dev, ABS_MT_POSITION_Y, input_y);
-	input_report_abs(ts->input_dev, ABS_MT_TOUCH_MAJOR, input_w);
-	input_report_abs(ts->input_dev, ABS_MT_WIDTH_MAJOR, input_w);
-
-	dev_info(&ts->pdev->dev, "point (%d %d %d)\n", input_x, input_y, input_w);
-}
-
-/**
- * semidrive_process_events - Process incoming events
- *
- * @ts: our semidrive_sts_data pointer
- * @point_data: our pointe data
- *
- * Called when the IRQ is triggered. Read the current device state, and push
- * the input events to the user space.
- */
 static void
 semidrive_process_events(struct semidrive_sts_data *ts, u8 *point_data)
 {
-	int touch_num;
 	int i;
+	struct ts_report_data *report_data = (struct ts_report_data *)point_data;
 
-	touch_num = semidrive_read_input_report(ts, point_data);
-	if ((touch_num < 0) || (point_data[0] == 0))
-		return;
+	input_report_key(ts->input_dev, KEY_LEFTMETA, report_data->key_value);
 
-	/*
-	 * Bit 4 of the first byte reports the status of the capacitive
-	 * Windows/Home button.
-	 */
-	input_report_key(ts->input_dev, KEY_LEFTMETA, point_data[0] & BIT(4));
+	for (i = 0; i < report_data->touch_num; i++) {
+		int id = report_data->coord_data[i].id;
+		int input_x = report_data->coord_data[i].x;
+		int input_y = report_data->coord_data[i].y;
+		int input_w = report_data->coord_data[i].w;
 
-	for (i = 0; i < touch_num; i++)
-		semidrive_sts_report_touch(ts,
-				&point_data[1 + STS_CONTACT_SIZE * i]);
+		input_x -= ts->x_offset;
+		input_y -= ts->y_offset;
+		if ((input_x < 0) || (input_x > ts->abs_x_max))
+			return;
+		if ((input_y < 0) || (input_y > ts->abs_y_max))
+			return;
+
+		/* Inversions have to happen before axis swapping */
+		if (ts->inverted_x)
+			input_x = ts->abs_x_max - input_x;
+		if (ts->inverted_y)
+			input_y = ts->abs_y_max - input_y;
+		if (ts->swapped_x_y)
+			swap(input_x, input_y);
+
+		input_mt_slot(ts->input_dev, id);
+		input_mt_report_slot_state(ts->input_dev, MT_TOOL_FINGER, true);
+		input_report_abs(ts->input_dev, ABS_MT_POSITION_X, input_x);
+		input_report_abs(ts->input_dev, ABS_MT_POSITION_Y, input_y);
+		input_report_abs(ts->input_dev, ABS_MT_TOUCH_MAJOR, input_w);
+		input_report_abs(ts->input_dev, ABS_MT_WIDTH_MAJOR, input_w);
+	}
 
 	input_mt_sync_frame(ts->input_dev);
 	input_sync(ts->input_dev);
@@ -187,16 +171,10 @@ static void semidrive_free_irq(struct semidrive_sts_data *ts)
 	mbox_free_channel(ts->mbox);
 }
 
-static void sts_work_handler(struct work_struct *work)
-{
-	//TODO: send frontent to trigger irq by event channel
-	pr_err("%s: Not implemented\n", __func__);
-}
-
 static void sts_coor_updated(struct mbox_client *client, void *mssg)
 {
 	struct semidrive_sts_data *ts = container_of(client,
-						struct semidrive_sts_data, client);
+				struct semidrive_sts_data, client);
 	struct platform_device *pdev = ts->pdev;
 	sd_msghdr_t *msghdr = mssg;
 
@@ -206,8 +184,6 @@ static void sts_coor_updated(struct mbox_client *client, void *mssg)
 	}
 
 	semidrive_process_events(ts, msghdr->data);
-
-	return;
 }
 
 static int semidrive_request_irq(struct semidrive_sts_data *ts)
@@ -215,8 +191,6 @@ static int semidrive_request_irq(struct semidrive_sts_data *ts)
 	struct platform_device *pdev = ts->pdev;
 	struct mbox_client *client;
 	int ret = 0;
-
-	INIT_WORK(&ts->rx_work, sts_work_handler);
 
 	client = &ts->client;
 	/* Initialize the mbox channel used by touchscreen */
@@ -237,7 +211,7 @@ static int semidrive_request_irq(struct semidrive_sts_data *ts)
 }
 
 static int semidrive_sts_ioctl(struct semidrive_sts_data *ts, int command,
-								struct sts_ioctl_result *data)
+	struct sts_ioctl_result *data)
 {
 	struct rpc_ret_msg result = {0,};
 	struct rpc_req_msg request;
@@ -248,7 +222,6 @@ static int semidrive_sts_ioctl(struct semidrive_sts_data *ts, int command,
 	DCF_INIT_RPC_REQ(request, MOD_RPC_REQ_STS_IOCTL);
 	ctl->op = command;
 	ctl->instance = ts->instance;
-	dev_err(&ts->pdev->dev, "%s():[%d]\n", __func__, ctl->instance);
 	ret = semidrive_rpcall(&request, &result);
 	if (ret < 0 || result.retcode < 0) {
 		dev_err(&ts->pdev->dev, "rpcall failed=%d %d\n", ret, result.retcode);
@@ -260,17 +233,6 @@ static int semidrive_sts_ioctl(struct semidrive_sts_data *ts, int command,
 fail_call:
 
 	return ret;
-}
-
-/**
- * semidrive_reset - Reset device during power on
- *
- * @ts: semidrive_sts_data pointer
- */
-static int semidrive_reset(struct semidrive_sts_data *ts)
-{
-	/* TODO: */
-	return 0;
 }
 
 static int semidrive_set_inited(struct semidrive_sts_data *ts)
@@ -288,16 +250,14 @@ static int semidrive_set_inited(struct semidrive_sts_data *ts)
  *
  * Must be called during probe
  */
-static void semidrive_read_config(struct semidrive_sts_data *ts)
+static int semidrive_read_config(struct semidrive_sts_data *ts)
 {
 	struct sts_ioctl_result op_ret;
 	int ret;
 
 	ret = semidrive_sts_ioctl(ts, STS_OP_GET_CONFIG, &op_ret);
-	dev_info(&ts->pdev->dev, "%s: ret=%d\n", __func__, ret);
-	if (ret) {
+	if (ret)
 		goto apply_default;
-	}
 
 	ts->abs_x_max = op_ret.msg.gc.abs_x_max;
 	ts->abs_y_max = op_ret.msg.gc.abs_y_max;
@@ -305,24 +265,40 @@ static void semidrive_read_config(struct semidrive_sts_data *ts)
 		swap(ts->abs_x_max, ts->abs_y_max);
 	ts->x_offset = op_ret.msg.gc.x_offset;
 	ts->y_offset = op_ret.msg.gc.y_offset;
-	ts->max_touch_num = op_ret.msg.gc.touch_num;
+	ts->max_touch_num = op_ret.msg.gc.max_touch_num;
+	ts->swapped_x_y = op_ret.msg.gc.swapped_x_y;
+	ts->inverted_x = op_ret.msg.gc.inverted_x;
+	ts->inverted_y = op_ret.msg.gc.inverted_y;
+
 	if (!ts->abs_x_max || !ts->abs_y_max || !ts->max_touch_num) {
+		dev_err(&ts->pdev->dev, "invalid: x_max=%d, y_max=%d, max_touch_num=%d\n",
+			ts->abs_x_max, ts->abs_y_max, ts->max_touch_num);
+		ret = 0;
 		goto apply_default;
 	}
-	dev_err(&ts->pdev->dev, "(%d-%d)[%d]\n", ts->abs_x_max, ts->abs_y_max, ts->max_touch_num);
 
-	return;
+	dev_err(&ts->pdev->dev, "(%d-%d)[%d]\n",
+		ts->abs_x_max, ts->abs_y_max, ts->max_touch_num);
+
+	return 0;
 
 apply_default:
-	dev_err(&ts->pdev->dev, "Invalid config, using defaults\n");
-	ts->abs_x_max = STS_MAX_WIDTH;
-	ts->abs_y_max = STS_MAX_HEIGHT;
-	if (ts->swapped_x_y)
-		swap(ts->abs_x_max, ts->abs_y_max);
-	ts->max_touch_num = STS_MAX_CONTACTS;
-	ts->x_offset = 0;
-	ts->y_offset = 0;
-	dev_err(&ts->pdev->dev, "(%d-%d)[%d]\n", ts->abs_x_max, ts->abs_y_max, ts->max_touch_num);
+	if (ts->abs_x_max != STS_MAX_WIDTH || ts->abs_y_max != STS_MAX_HEIGHT) {
+		ts->abs_x_max = STS_MAX_WIDTH;
+		ts->abs_y_max = STS_MAX_HEIGHT;
+		if (ts->swapped_x_y)
+			swap(ts->abs_x_max, ts->abs_y_max);
+		ts->max_touch_num = STS_MAX_CONTACTS;
+		ts->x_offset = 0;
+		ts->y_offset = 0;
+		ts->swapped_x_y = 0;
+		ts->inverted_x = 0;
+		ts->inverted_y = 0;
+
+		dev_err(&ts->pdev->dev, "use default (%d-%d)[%d]\n",
+			ts->abs_x_max, ts->abs_y_max, ts->max_touch_num);
+	}
+	return ret;
 }
 
 /**
@@ -337,16 +313,22 @@ static int semidrive_read_version(struct semidrive_sts_data *ts)
 
 	ret = semidrive_sts_ioctl(ts, STS_OP_GET_VERSION, &op_ret);
 	if (ret < 0) {
-		ts->id = 0x1001;
-		dev_err(&ts->pdev->dev, "read version failed: %d\n", ret);
+		if (ts->version != STS_VERSION || ts->id != STS_ID || ts->vendor != STS_VENDOR) {
+			ts->version = STS_VERSION;
+			ts->id = STS_ID;
+			ts->vendor = STS_VENDOR;
+			dev_err(&ts->pdev->dev, "use default id: %d, version: %04x, vendor: %d\n",
+				ts->id, ts->version, ts->vendor);
+		}
 		return ret;
 	}
 
 	ts->version = op_ret.msg.v.version;
 	ts->id = op_ret.msg.v.id;
-	ts->dev_type = op_ret.msg.v.dev_type;
+	ts->vendor = op_ret.msg.v.vendor;
 
-	dev_err(&ts->pdev->dev, "ID %d, version: %04x, type: %d\n", ts->id, ts->version, ts->dev_type);
+	dev_err(&ts->pdev->dev, "id: %d, version: %04x, vendor: %d\n",
+		ts->id, ts->version, ts->vendor);
 
 	return 0;
 }
@@ -369,31 +351,33 @@ static int semidrive_register_input_dev(struct semidrive_sts_data *ts)
 	}
 
 	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_X,
-			     0, ts->abs_x_max, 0, 0);
+		0, ts->abs_x_max, 0, 0);
 	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_Y,
-			     0, ts->abs_y_max, 0, 0);
-	input_set_abs_params(ts->input_dev, ABS_MT_WIDTH_MAJOR, 0, 255, 0, 0);
-	input_set_abs_params(ts->input_dev, ABS_MT_TOUCH_MAJOR, 0, 255, 0, 0);
+		0, ts->abs_y_max, 0, 0);
+	input_set_abs_params(ts->input_dev, ABS_MT_WIDTH_MAJOR,
+		0, 255, 0, 0);
+	input_set_abs_params(ts->input_dev, ABS_MT_TOUCH_MAJOR,
+		0, 255, 0, 0);
 
 	input_mt_init_slots(ts->input_dev, ts->max_touch_num,
 			    INPUT_MT_DIRECT | INPUT_MT_DROP_UNUSED);
 
 	ts->input_dev->name = "Semidrive Safe TouchScreen";
-	if (ts->dev_type == 1)
-		ts->input_dev->phys = "input/ts_aux";
-	else
+	if (ts->instance == TS_SUPPORT_ANRDOID_MAIN)
 		ts->input_dev->phys = "input/ts_main";
+	else
+		ts->input_dev->phys = "input/ts_aux";
+
 	ts->input_dev->id.bustype = BUS_I2C;
-	ts->input_dev->id.vendor = 0x0416;
+	ts->input_dev->id.vendor = ts->vendor;
 	ts->input_dev->id.product = ts->id;
 	ts->input_dev->id.version = ts->version;
 
 	/* Capacitive Windows/Home button on some devices */
 	input_set_capability(ts->input_dev, EV_KEY, KEY_LEFTMETA);
 	ret = input_register_device(ts->input_dev);
-	if (ret) {
+	if (ret)
 		dev_err(&ts->pdev->dev, "Failed to register input device: %d", ret);
-	}
 
 	return ret;
 }
@@ -413,15 +397,6 @@ static int semidrive_configure_dev(struct semidrive_sts_data *ts)
 	struct platform_device *pdev = ts->pdev;
 	int ret = 0;
 
-	ts->swapped_x_y = device_property_read_bool(&pdev->dev,
-						    "touchscreen-swapped-x-y");
-	ts->inverted_x = device_property_read_bool(&pdev->dev,
-						   "touchscreen-inverted-x");
-	ts->inverted_y = device_property_read_bool(&pdev->dev,
-						   "touchscreen-inverted-y");
-
-	semidrive_read_config(ts);
-
 	ret = semidrive_register_input_dev(ts);
 	if (ret)
 		return ret;
@@ -431,10 +406,41 @@ static int semidrive_configure_dev(struct semidrive_sts_data *ts)
 	return ret;
 }
 
+static void sts_work_handler(struct work_struct *work)
+{
+	int ret;
+
+	struct semidrive_sts_data *ts =
+		container_of(work, struct semidrive_sts_data, ts_work.work);
+
+	if (!ts->read_version_stat) {
+		ret = semidrive_read_version(ts);
+		if (!ret)
+			ts->read_version_stat = true;
+	}
+
+	if (!ts->read_config_stat) {
+		ret = semidrive_read_config(ts);
+		if (!ret)
+			ts->read_config_stat = true;
+	}
+
+	ts->ts_work_count++;
+
+	if ((ts->read_version_stat && ts->read_config_stat) || ts->ts_work_count >= 4) {
+		cancel_delayed_work(&ts->ts_work);
+		dev_err(&ts->pdev->dev, "read_version_stat=%d, read_config_stat=%d\n",
+			ts->read_version_stat, ts->read_config_stat);
+	} else {
+		schedule_delayed_work(&ts->ts_work, msecs_to_jiffies(500));
+	}
+}
+
 static int semidrive_sts_probe(struct platform_device *pdev)
 {
 	struct semidrive_sts_data *ts;
 	int ret;
+	u16 instance;
 
 	ts = devm_kzalloc(&pdev->dev, sizeof(*ts), GFP_KERNEL);
 	if (!ts)
@@ -444,23 +450,32 @@ static int semidrive_sts_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, ts);
 
 	/* Read instance from dts */
-	ret = of_property_read_u16(pdev->dev.of_node, "touchscreen-id", &ts->instance);
+	ret = of_property_read_u16(pdev->dev.of_node, "touchscreen-id", &instance);
 	if (ret < 0) {
-		dev_err(&pdev->dev, "Missing touchscreen id, use 0\n");
-		ts->instance = 0;
-	}
-
-	ret = semidrive_reset(ts);
-	if (ret) {
-		dev_err(&pdev->dev, "reset failed: %d\n", ret);
-		goto err;
+		dev_err(&pdev->dev, "Warning: Missing touchscreen id\n");
+		ts->instance = TS_SUPPORT_ANRDOID_MAIN;
+	} else {
+		if (instance == 0)
+			ts->instance = TS_SUPPORT_ANRDOID_MAIN;
+		else if (instance == 1)
+			ts->instance = TS_SUPPORT_ANRDOID_AUX1;
+		else if (instance == 2)
+			ts->instance = TS_SUPPORT_ANRDOID_AUX2;
+		else if (instance == 3)
+			ts->instance = TS_SUPPORT_ANRDOID_AUX3;
 	}
 
 	ret = semidrive_read_version(ts);
-	if (ret) {
-		dev_err(&pdev->dev, "Read version failed %d\n", ret);
-		goto err;
-	}
+	if (ret)
+		dev_err(&pdev->dev, "%s, Read version failed %d\n", __func__, ret);
+	else
+		ts->read_version_stat = true;
+
+	ret = semidrive_read_config(ts);
+	if (ret)
+		dev_err(&pdev->dev, "%s, Read config failed %d\n", __func__, ret);
+	else
+		ts->read_config_stat = true;
 
 	ret = semidrive_configure_dev(ts);
 	if (ret) {
@@ -473,6 +488,9 @@ static int semidrive_sts_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "set init failed %d\n", ret);
 		goto err;
 	}
+
+	INIT_DELAYED_WORK(&ts->ts_work, sts_work_handler);
+	schedule_delayed_work(&ts->ts_work, msecs_to_jiffies(500));
 
 	dev_err(&pdev->dev, "%s(): done ok\n", __func__);
 
@@ -488,7 +506,6 @@ static int semidrive_sts_remove(struct platform_device *pdev)
 	struct semidrive_sts_data *ts = platform_get_drvdata(pdev);
 
 	semidrive_free_irq(ts);
-
 	return 0;
 }
 
