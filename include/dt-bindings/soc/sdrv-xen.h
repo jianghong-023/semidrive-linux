08c8cd18b6e8220be985e39875a78264137e33e0
liang.liu <liang.liu@semidrive.com>
2021-07-31 18:13:18 +0800

    xen: update xen dt-bindings
    reassigned xen passthrough master devices' sid value
    
    ID: 2695
    Change-Id: I72db5393c3516a6d53c9166ce87a90c06d7d94f0

--- include/dt-bindings/soc/sdrv-xen.h
+++ include/dt-bindings/soc/sdrv-xen.h
@@ -2,28 +2,27 @@
 #ifndef __DT_BINDINGS_SDRV_XEN_H
 #define __DT_BINDINGS_SDRV_XEN_H
 
-/* sid */
-#define SDRV_SID_DMAC2        0x72
-#define SDRV_SID_DMAC3        0x73
-#define SDRV_SID_DMAC4        0x74
-#define SDRV_SID_DMAC5        0x75
-#define SDRV_SID_DMAC6        0x76
-#define SDRV_SID_DMAC7        0x77
+/* sid max:0x7f */
 
 #define SDRV_SID_CRTC0        0x01
 #define SDRV_SID_CRTC1        0x02
 #define SDRV_SID_CRTC2        0x03
 #define SDRV_SID_CRTC3        0x04
 
-#define SDRV_SID_SDHCI3       0x62
+#define SDRV_SID_G2D1         0x05
+#define SDRV_SID_G2D2         0x06
 
-#define SDRV_SID_USB0         0x30
-#define SDRV_SID_USB1         0x31
+#define SDRV_SID_DMAC2        0x07
+#define SDRV_SID_DMAC3        0x08
+#define SDRV_SID_DMAC4        0x09
+#define SDRV_SID_DMAC5        0x0A
+#define SDRV_SID_DMAC6        0x0B
+#define SDRV_SID_DMAC7        0x0C
 
-#define SDRV_SID_ETH2         0x50
+#define SDRV_SID_SDHCI3       0x0D
 
-#define SDRV_SID_VPU1         0x40
-#define SDRV_SID_VPU2         0x41
+#define SDRV_SID_USB0         0x0E
+#define SDRV_SID_USB1         0x0F
 
 #define SDRV_SID_GPU0         0x10
 #define SDRV_SID_GPU0_0       0x11
@@ -37,16 +36,22 @@
 #define SDRV_SID_GPU1_2       0x23
 #define SDRV_SID_GPU1_3       0x24
 
-#define SDRV_SID_CSI0         0x80
-#define SDRV_SID_CSI1         0x81
-#define SDRV_SID_CSI2         0x82
+#define SDRV_SID_ETH2         0x30
+#define SDRV_SID_ETH2         0x31
 
-#define SDRV_SID_PCIE0        0x90
+#define SDRV_SID_VPU1         0x32
+#define SDRV_SID_VPU2         0x33
 
-#define SDRV_SID_PCIE1        0xa0
 
-#define SDRV_SID_G2D1         0x05
-#define SDRV_SID_G2D2         0x06
+#define SDRV_SID_CSI0         0x34
+#define SDRV_SID_CSI1         0x35
+#define SDRV_SID_CSI2         0x36
+
+/* 0x40 - 0x4F */
+#define SDRV_SID_PCIE0        0x40
+/* 0x50 - 0x57 */
+#define SDRV_SID_PCIE1        0x50
+
 
 /* vm dtb */
 #define GUEST_PHANDLE_GIC (65000)
